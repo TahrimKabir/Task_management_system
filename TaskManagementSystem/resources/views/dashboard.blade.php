d342bb1091b010440fe52671c7e999c4acb173dc
TahrimKabir <tahrimkabir321@gmail.com>
2024-12-25 13:50:23 +0600

    authentication done

--- TaskManagementSystem/resources/views/dashboard.blade.php
+++ TaskManagementSystem/resources/views/dashboard.blade.php
@@ -1,44 +1,17 @@
-<!DOCTYPE html>
-<html lang="en"> <!--begin::Head-->
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 
-<head>
-    @include('layouts.cssLink')
-</head> <!--end::Head--> <!--begin::Body-->
-
-<body class="layout-fixed sidebar-expand-lg bg-body-tertiary"> <!--begin::App Wrapper-->
-@section('app-wrapper')   
-<div class="app-wrapper"> 
-        <!--begin::Header-->
-        @include('layouts.header')
-        <!--end::Header--> 
-        <!--begin::Sidebar-->
-        @include('layouts.left_side_bar')
-       <!--end::Sidebar--> 
-        <!--begin::App Main-->
-        <main class="app-main"> <!--begin::App Content Header-->
-            <div class="app-content-header"> <!--begin::Container-->
-                <div class="container-fluid"> <!--begin::Row-->
-                    @section('edit-header')
-                    @yield('edit-content-header')
-                    
-                    @show<!--end::Row-->
-                </div> <!--end::Container-->
-            </div> <!--end::App Content Header--> <!--begin::App Content-->
-            <div class="app-content"> <!--begin::Container-->
-            @section('edit-body')
-                <div class="container-fluid"> <!--begin::Row-->
-                    
-                    @yield('edit-content-body')
-                    
-                    <!-- /.row (main row) -->
-                </div> <!--end::Container-->
-            @show
-            </div> <!--end::App Content-->
-        </main> <!--end::App Main--> <!--begin::Footer-->
-        @include('layouts.footer') <!--end::Footer-->
-    </div> <!--end::App Wrapper--> <!--begin::Script--> <!--begin::Third Party Plugin(OverlayScrollbars)-->
-  @show
-    @include('layouts.script')<!--end::Script-->
-</body><!--end::Body-->
-
-</html>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
